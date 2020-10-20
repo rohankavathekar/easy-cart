c9618f45a0815f31ba445d0884bc275969ea77da
Rohan Kavathekar <rohan.kavathekar@wgblindia.com>
2020-10-21 02:44:02 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import { Switch, Route } from 'react-router-dom'
+import Navigation from './components/Navigation';
 import Catagories from './components/Catagories';
 import Products from './components/Products';
 import Details from './components/Details';
 import Cart from './components/Cart';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
+import './App.css'; 
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Catagories/>
-      <Products/>
-      <Details/>
-      <Cart/>
+      <Navigation/>
+      <Switch>
+        <Route exact path="/" component={Catagories} />
+        <Route path="/products" component={Products} />
+        <Route path="/details" component={Details} />
+        <Route path="/cart" component={Cart} />
+      </Switch>
     </div>
   );
 };
---
+++ src/components/Navigation.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {link} from 'react-router-dom';
+import {Navbar,Nav} from 'react-bootstrap';
+
+const Navigation = () => {
+  return (
+    <div>
+      <Navbar className="navbar" collapseOnSelect expand="lg">
+        <Navbar.Brand href="#home">
+          <img
+            src="/logo.png"
+            width="80"
+            height="80"
+            className="d-inline-block align-top"
+            alt="EasyCart"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#pricing">
+            <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+             My Cart</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Navigation;
