dd3288774b8b2213a88f17561ec98f56956142f5
Rohan Kavathekar <rohan.kavathekar@wgblindia.com>
2020-10-22 01:50:29 +0530

    context API

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import Title from "./Title"
+import React, { Component } from 'react';
+import Title from "./Title";
+import Product from './Product';
+import { ProductConsumer } from '../context';
 
-const Products = () => {
-
-  return (
-    <div>
-      <div className="mtb-50">
-        <div className="container">
-          <Title name = "product Name"/>
-          <div className="row">
-        
+export default class Products extends Component {
+  render() {
+    return (
+      <div>
+        <div className="mtb-50">
+          <div className="container">
+            <Title name="product"/>
+              <div className="row">
+                <ProductConsumer>
+                  {
+                    (value)=>{
+                    return value.products.map(product =>{
+                      return <Product key={product.id} product={product}/>
+                    })
+                    }
+                  }
+                </ProductConsumer>
+              </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-export default Products;
+    )
+  }
+}
